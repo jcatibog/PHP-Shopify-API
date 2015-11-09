9ad6e20464e9dcbb840994870089e58b9ae197fc
Jordan Catibog <jcatibog@cdsglobal.ca>
2015-11-09 16:12:29 -0500

    CS fixes

--- src/Shopify/Client.php
+++ src/Shopify/Client.php
@@ -2,34 +2,38 @@
 
 namespace Shopify;
 
+use Zend\Http\Request;
+
 class Client
 {
     const USER_AGENT = 'PHP Shopify API v0.0.1';
 
-    private $_client = null;
+    private $client = null;
+    private $request = null;
 
-    private $_config = array(
+    private $config = array(
         'api_key' => '',
         'secret'  => '',
         'shop'    => '',
         'token'   => ''
     );
 
-    private $_url = '';
 
     public function __construct($config = null)
     {
         if ($config !== null) {
             $this->setConfig($config);
         }
+
+        $this->request = new \Zend\Http\Request();
     }
 
     public function request($request = null)
     {
         if (is_null($request)) {
             throw new \InvalidArgumentException('You must specify a request url.');
-        } elseif (!is_string($request)) {
-            throw new \InvalidArgumentException('String expected, got ' . gettype($request));
+        } elseif (!is_string($request) || !($request instanceof \Zend\Http\Request)) {
+            throw new \InvalidArgumentException('String or \\Zend\\Http\\Request object expected, got ' . gettype($request));
         }
 
         $this->_getClient()->setUri($this->_getUrl() . $request);
@@ -37,36 +41,36 @@ class Client
         return json_decode($this->_getClient()->getResponse()->getBody());
     }
 
-    private function _getClient()
+    private function getClient()
     {
-        if (!is_object($this->_client) || !$this->_client instanceof \Zend\Http\Client) {
+        if (!is_object($this->client) || !($this->client instanceof \Zend\Http\Client)) {
             $this->_client = new \Zend\Http\Client();
         }
 
         return $this->_client;
     }
 
-    private function _getUrl()
+    private function getUrl()
     {
-        if (!isset($this->_config['api_key'])
-            || $this->_config['api_key'] == ''
+        if (!isset($this->config['api_key'])
+            || $this->config['api_key'] == ''
         ) {
             throw new \Exception('Api key not set!');
         }
 
-        if (!isset($this->_config['secret'])
-            || $this->_config['secret'] == ''
+        if (!isset($this->config['secret'])
+            || $this->config['secret'] == ''
         ) {
             throw new \Exception('Secret not set!');
         }
 
-        if (!isset($this->_config['shop'])
-            || $this->_config['shop'] == ''
+        if (!isset($this->config['shop'])
+            || $this->config['shop'] == ''
         ) {
             throw new \Exception('Shop not set!');
         }
 
-        return 'https://'. $this->_config['api_key'] .':'. md5($this->_config['secret'] . $this->_config['token']) .'@'. $this->_config['shop'];
+        return 'https://'. $this->config['api_key'] .':'. md5($this->config['secret'] . $this->config['token']) .'@'. $this->config['shop'];
     }
 
     public function setConfig($config = array())
@@ -79,7 +83,7 @@ class Client
         }
 
         foreach ($config as $k => $v) {
-            $this->_config[strtolower($k)] = $v;
+            $this->config[strtolower($k)] = $v;
         }
 
         return $this;
